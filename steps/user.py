b25e07cbff64a98dbf14680379184da75b391005
Lera <vallery_ostanko@mail.ru>
2022-08-23 23:52:04 +0300

    Done:create base_step

--- steps/user.py
+++ steps/user.py
@@ -1,27 +1,13 @@
-import json
-
 import allure
-from requests import Request, Response, session
+from requests import Request, Response
 
 from settings import API_HOST
+from steps.base_step import BaseApiSteps
 
 
-class UsersApiSteps:
+class UsersApiSteps(BaseApiSteps):
     _user_endpoint = "/user/"
     _user_by_id_endpoint = "/user/{user_id}"
-
-    def send_request(self, req: Request) -> Response:
-        with allure.step(f"Request {req.method}: {req.url}"):
-            allure.attach(json.dumps(req.headers, indent=4), "headers")
-
-        prepared_req = session().prepare_request(req)
-        r: Response = session().send(prepared_req)
-        with allure.step(f"Response: {r.status_code}"):
-            allure.attach(json.dumps(dict(r.headers), indent=4), "headers")
-            if r.content:
-                if r.json():
-                    allure.attach(json.dumps(r.json(), indent=4), "body")
-        return r
 
     @allure.step(f" GET{_user_endpoint}")
     def get_user(self) -> Response:
